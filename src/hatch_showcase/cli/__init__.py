8d4672a9f7d6af3d9a417aa4611e893648b17f8a
Ofek Lev <ofekmeister@gmail.com>
2023-05-10 01:36:37 -0400

    Fix CI (#15)
    * Fix CI
    
    * .
    
    * .
    
    * Update build.yml
    
    * .

--- src/hatch_showcase/cli/__init__.py
+++ src/hatch_showcase/cli/__init__.py
@@ -6,7 +6,6 @@ import click
 from hatch_showcase._version import version
 from hatch_showcase.fib import fibonacci
 
-
 # NOTE: The group/command decorators must come last to avoid the following issue at runtime:
 # https://github.com/pallets/click/issues/1199
 
